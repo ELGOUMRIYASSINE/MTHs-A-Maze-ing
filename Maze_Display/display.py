94127f6af236c0bdbe1a9f81755283458f42804f
Ayoub Fadli <ayfadli@c3r9p5.1337.ma>
2026-02-03 12:02:21 +0100

    Enhancing the style of maze by the combination of the wall of two neighbors, this make the maze more readable and clear.

--- Maze_Display/display.py
+++ Maze_Display/display.py
@@ -1,52 +1,58 @@
 import time
 
-def render_maze(string_hex = "FAF5FFF"):
-    CHAR_V = "║"
-    CHAR_H = "═"
-    # Corners (For F=1111 looks)
-    C_TL = "╔" # Top Left
-    C_TR = "╗" # Top Right
-    C_BL = "╚" # Bottom Left
-    C_BR = "╝" # Bottom Right
-    SPACE = " "
-    maze_matrix = []
+def parse_to_matrix(filename):
+    matrix = []
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                clean_line = line.strip()
+                if not clean_line: continue
 
-    line_top = ""
-    line_mid1 = ""
-    line_mid2 = ""
+                row = []
+                for c in clean_line:
+                    value = int(c, 16)
+                    value = format(value, "04b")
+                    bits = [int(x) for x in value]
+                    row.append(bits)
+                matrix.append(row)
+
+    except FileNotFoundError:
+        print("Error: File not found")
+        return []
+
+    return matrix
+
+CHAR = "█"
+SPACE = " "
+
+def render_maze():
+    matrix = parse_to_matrix("../maze_output.txt")
+    for row in matrix:
+        line_top = ""
+        line_mid = ""
+        for cell in row:
+            west, south, east, north = cell
+            line_top += CHAR
+            line_top += CHAR * 3 if north else SPACE * 3
+
+            line_mid += CHAR if west else SPACE
+            line_mid += SPACE * 3
+
+        line_top += CHAR
+        line_mid += CHAR
+
+        print(line_top)
+        print(line_mid)
+
     line_bot = ""
-    CELL_WIDTH = 6
+    last_row = matrix[-1]
 
-    for c in string_hex:
-        val = int(c, 16)
-        binary_string = format(val, '04b')
-        bit_list = [int(bit) for bit in binary_string]
-        maze_matrix.append(bit_list)
-    for bits in maze_matrix:
-        west, south, east, north = bits
-        middle_part = CHAR_H * 6 if north else SPACE * CELL_WIDTH
-        line_top += f"{C_TL}{middle_part}{C_TR}"
+    for cell in last_row:
+        west, south, east, north = cell
+        line_bot += CHAR
+        line_bot += CHAR * 3 if south else SPACE * 3
 
-        line_mid1 += f"{CHAR_V}{SPACE * 6}" if west else f"{SPACE * 7}"
-        line_mid1 += f"{CHAR_V}" if east else f"{SPACE}"
-
-        line_mid2 += f"{CHAR_V}{SPACE * 6}" if west else f"{SPACE * 7}"
-        line_mid2 += f"{CHAR_V}" if east else f"{SPACE}"
-
-        middle_part = CHAR_H * 6 if south else SPACE * CELL_WIDTH
-        line_bot += f"{C_BL}{middle_part}{C_BR}"
-    print(line_top)
-    print(line_mid1)
-    print(line_mid2)
+    line_bot += CHAR
     print(line_bot)
 
-def maze_parser():
-    try:
-        with open("../maze_output.txt", 'r') as file:
-            line = file.readline()
-            while line:
-                render_maze(line.strip())
-                line = file.readline()
-    except FileNotFoundError:
-        print("File: 'maze_output.txt' not found !")
-maze_parser()
+render_maze()
--- Maze_Display/test.py
+++ Maze_Display/test.py
@@ -1,10 +1,19 @@
-value = int("A", 16)
-value = format(value, '04b')
-print(value)
-value = [int(x) for x in value]
-west, south, east, north = value
-print(west)
-print(south)
-print(east)
-print(north)
+# A 3D Array (2 Floors, 2 Apts, 2 Rooms)
+building = [
+    # Floor 0
+    [
+        [1, 2],  # Apt 0
+        [3, 4]   # Apt 1 (Task: Fill this with 3 and 4)
+    ],
+    # Floor 1
+    [
+        [5, 6],  # Apt 0 (Task: Fill this with 5 and 6)
+        [7, 8]   # Apt 1
+    ]
+]
 
+for f in building:
+    for a in f:
+        for r in a:
+            print(f"[{r}]", end = "")
+# print(building[1][1][1])
